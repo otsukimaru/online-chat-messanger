77c38c426df29bd46b57bcec971d3ae8a8df5b74
otsukimaru <otsukinandesu@gmail.com>
2024-03-05 23:12:17 +0900

    リレーシステム追加

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
 import socket
-import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('127.0.0.1', 9001)
@@ -9,10 +8,12 @@ if user_name:
     print('login:' + user_name)
     while True:
         message = input('enter message: ')
+        
         user_name_bits = user_name.encode('utf-8')
         message_bits = message.encode('utf-8')
         header = len(user_name_bits).to_bytes(1, 'big') + user_name_bits
         sock.sendto(header + message_bits, server_address)
+        
         data, server = sock.recvfrom(4096)
         print(data.decode('utf-8'))
 else:
--- server.py
+++ server.py
@@ -6,10 +6,12 @@ server_address = '0.0.0.0'
 server_port = 9001
 
 sock.bind((server_address, server_port))
+user_arg_set = set()
 
 try:
     while True:
         header, client_address = sock.recvfrom(4096)
+        user_arg_set.add(client_address)
 
         if header:
             user_name_length = int.from_bytes(header[:1], 'big')
@@ -19,7 +21,10 @@ try:
             if user_name_length == 0:
                 sock.sendto('not allow empty of user name')
                 raise Exception('user name is empty')
+            
             print('login:' + user_name.decode('utf-8'))
-            sent = sock.sendto(message, client_address)
+            for val in user_arg_set:
+                if val != client_address:
+                    sent = sock.sendto(message, val)
 finally:
     sock.close()
